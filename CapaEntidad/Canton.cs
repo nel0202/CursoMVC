f9976c38576301e679a7881b15c40fec667673cd
Nelson Calderon <152829010+nel0202@users.noreply.github.com>
2024-07-02 15:16:00 -0600

    Agregar archivos de proyecto.

---
+++ CapaEntidad/Canton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+	public class Canton
+	{
+        public int IdCanton { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
